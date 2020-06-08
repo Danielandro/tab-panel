16f14de85cfdde05aedb368ea07750d29a76e5b7
Danny <mtme13@hotmail.com>
2020-06-08 19:07:37 +0100

    Dynamically pass tab names * use @ContentChildren to retrieve tabs * loop through tabs to grab + display title

--- src/app/tab-panel/tab-panel.component.ts
+++ src/app/tab-panel/tab-panel.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ContentChildren, QueryList, AfterContentInit } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
 
 @Component({
   selector: 'app-tab-panel',
   templateUrl: './tab-panel.component.html',
   styleUrls: ['../tab-panel.component.scss']
 })
-export class TabPanelComponent implements OnInit {
-
+export class TabPanelComponent implements AfterContentInit {
+  // grab child components of this class
+  // QueryList is iterable i.e. works with ngFor
+  @ContentChildren(TabComponent)
+  tabs: QueryList<TabComponent>;
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterContentInit(): void {
+    console.log('TABS ', this.tabs);
   }
 
 }
