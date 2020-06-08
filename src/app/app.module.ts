ea07416084ca9be49b2e50f37e440f4a383757a1
Danny <mtme13@hotmail.com>
2020-06-08 15:33:17 +0100

    Copy example code from project * template for tab panel * styles for app, tab-panel & theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { TabPanelComponent } from './tab-panel/tab-panel.component';
+import { TabComponent } from './tab/tab.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabPanelComponent,
+    TabComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
