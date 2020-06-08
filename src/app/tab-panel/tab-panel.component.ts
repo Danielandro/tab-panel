1d982d50d05a339a0b7b60485a92a4dd9fc4b958
Danny <mtme13@hotmail.com>
2020-06-08 17:55:43 +0100

    Extract html into tab & tab-panel components * also extract styles

--- src/app/tab-panel/tab-panel.component.ts
+++ src/app/tab-panel/tab-panel.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tab-panel',
   templateUrl: './tab-panel.component.html',
-  styleUrls: ['./tab-panel.component.scss']
+  styleUrls: ['../tab-panel.component.scss']
 })
 export class TabPanelComponent implements OnInit {
 
